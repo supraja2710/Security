d98c799350a7170386881ef75a470d3cd2578a48
Ganesh Iyer <lastlegion@gmail.com>
2017-06-05 15:04:56 +0000

    disabled authentication for this branch

--- authenticate.php
+++ authenticate.php
@@ -1,14 +1,17 @@
 <?php
 require_once('config/security_config.php');
 // start sessions
+session_start();
+
 
 /*
  * Disable authentication
+ */
  $_SESSION["api_key"] = "APIKEY312"; //Don't change this string, we do a find and replace to populate the actual api key here.
  $_SESSION["email"] = "viewer@quip"; //dummy user. 
- */
 
-session_start();
+
+
 if (!isset($_SESSION["api_key"])) {
     session_unset();
     header("Location:http://".$_SERVER["HTTP_HOST"].$folder_path."index.php");
