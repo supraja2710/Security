891902a26b5620366db498e9b571d1fcfafe7527
Ryan Birmingham <birm@rbirm.us>
2017-06-28 13:42:35 -0400

    try to force non blue link text

--- select.php
+++ select.php
@@ -35,7 +35,7 @@ $_SESSION["name"] = "quip";
         <div class="nav-wrapper">
           <a href="#!" class="brand-logo">
             <i class="microscope">
-              <img src="svg/camic_vector.svg" id="svg1" width="100%" height="100%" viewBox="0 0 640 480" preserveAspectRatio="xMaxYMax"/>
+              <img src="svg/camic_vector.svg" id="svg1" class="camic" width="100%" height="100%" viewBox="0 0 640 480" preserveAspectRatio="xMaxYMax"/>
             </i>
             <?php print $branding_title; ?>
           </a>
