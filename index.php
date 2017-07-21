5805b8db0401dad0665d5e8e1ba702b058be6856
Ryan Birmingham <birm@rbirm.us>
2017-07-21 18:34:29 -0400

    remove outdated style rule

--- index.php
+++ index.php
@@ -52,12 +52,6 @@
                 }
             }
           </script>
-          <style type="text/css">
-            body {
-            padding-top: 50px;
-            padding-bottom: 20px;
-            }
-          </style>
         </head>
         <body>
           <!--[if lt IE 8]>
