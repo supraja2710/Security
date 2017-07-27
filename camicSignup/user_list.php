8c8681bedad353fdb5b852ba2ee8bc461a609915
Ryan Birmingham <birm@rbirm.us>
2017-07-27 12:14:05 -0400

    fix logo

--- camicSignup/user_list.php
+++ camicSignup/user_list.php
@@ -37,7 +37,7 @@
         <div class="nav-wrapper">
           <a href="index.php" class="brand-logo">
             <i class="microscope">
-              <img src="svg/camic_vector.svg" id="svg1" class="camic_logo" width="100%" height="100%" viewBox="0 0 640 480" preserveAspectRatio="xMaxYMax"/>
+              <img src="../svg/camic_vector.svg" id="svg1" class="camic_logo" width="100%" height="100%" viewBox="0 0 640 480" preserveAspectRatio="xMaxYMax"/>
             </i>
             [*]<?php print $branding_title; ?>
           </a>
