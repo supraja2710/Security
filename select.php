4569dc84b4c512295ad27778f275d4fe488e1044
Ryan Birmingham <birm@rbirm.us>
2017-07-27 11:59:57 -0400

    fix wrong photo extension in select

--- select.php
+++ select.php
@@ -103,7 +103,7 @@ $_SESSION["name"] = "quip";
                 <div class="top_bar">About</div>
                 <div class="card-image">
                   <div class="darkimg">
-                    <img src="img/about.png">
+                    <img src="img/about.jpg">
                   </div>
                 </div>
                 <div class="card-content">
