8c05dd875ac63622a0c9fc936cded56bb46ece66
Ryan Birmingham <birm@rbirm.us>
2017-10-20 14:49:42 -0400

    add simpler login page, branding customization

--- branding.php
+++ branding.php
@@ -2,3 +2,12 @@
 // defaults
 
 $branding_title = "caMicroscope";
+
+$branding_footer = "U24 CA18092401A1, Tools to Analyze Morphology and Spatially Mapped Molecular Data; Joel Saltz PI Stony Brook/Emory/Oak Ridge/Yale
+NCIP/Leidos 14X138, caMicroscope – A Digital Pathology Integrative Query System; Ashish Sharma PI Emory/WUSTL/Stony Brook"
+
+$branding_description = "This is a restricted access dataset consisting of the various
+images and features acquired as part of a pilot project with the cancer registries.
+It is powered by a suite of tools called QuIP and caMicroscope that
+enable the exploration and analysis of whole slide images and pathomic features
+<br/>For more information please contact Ashish Sharma (ashish.sharma@emory.edu)"
