102c75d18bb247af3bd9ecac397a118946b70fa9
Ryan Birmingham <birm@rbirm.us>
2017-10-23 13:45:54 -0400

    remove contact info

--- branding.php
+++ branding.php
@@ -11,7 +11,6 @@ NCIP/Leidos 14X138, caMicroscope �~@~S A Digital Pathology Integrative Query S
 $branding_description = "This is a restricted access dataset consisting of the various
 images and features acquired as part of a pilot project with the cancer registries.
 It is powered by a suite of tools called QuIP and caMicroscope that
-enable the exploration and analysis of whole slide images and pathomic features
-<br/>For more information please contact Ashish Sharma (ashish.sharma@emory.edu)";
+enable the exploration and analysis of whole slide images and pathomic features";
 
 $download_link = "https://sbu-bmi.github.io/quip_distro/";
