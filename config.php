8bf8a52da5a7ef0faedb8d42c41ddfaf7e68655d
Ryan Birmingham <birm@rbirm.us>
2018-01-04 13:18:14 -0500

    streamline config

--- config.php
+++ config.php
@@ -10,4 +10,33 @@ try{
 catch(Exception $e){
 }
 
+// fail if any of following missing
+$api_key;
+$trusted_secret;
+
 // security default to on
+// if security off, add a warning band
+$disable_security;
+$mongo_client_url;
+$trusted_id;
+$trusted_url;
+$title;
+$suffix;
+$description;
+$footer;
+$download_link;
+
+return [
+  'config' => [
+    'api_key' => $api_key,
+    'trusted_secret' => $trusted_secret,
+    'disable_security' => $disable_security,
+    'mongo_client_url' => $mongo_client_url,
+    'trusted_id' => $trusted_id,
+    'title' => $title,
+    'suffix' => $suffix,
+    'description' => $description,
+    'footer' => $footer,
+    'download_link' => $download_link
+  ],
+];
